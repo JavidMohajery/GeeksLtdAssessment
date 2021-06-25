de91e729fddd4832848977929e948652f808c8cf
JMohajery <javidmohajery@gmail.com>
2021-06-25 06:45:42 +0430

    remove preloader animation

--- Domain/Game.cs
+++ Domain/Game.cs
@@ -9,23 +9,25 @@ namespace Domain
         private const int POINT_VALUE = 12;
         private const int PENALTY_VALUE = -5;
         private List<int> alreadyGeneratedCandidates = new List<int>();
+
         public int TotalPoints { get; set; }
         private Nationality GuessedNationality { get; set; }
         public Nationality ActualNationality { get; set; }
         public Candidate Candidate { get; set; }
         public int RoundNumber { get; set; }
-        
 
-        public void Guess(int id)
-        {
-            GuessedNationality = NationalityCollection.Nationalities.SingleOrDefault(x => x.Id == id);
-        }
+
         public void GenerateCandidate()
         {
             int id = GeneraterandomCandidateId();
             Candidate = CandidateCollection.Candidates.ElementAt(id);
 
             ActualNationality = NationalityCollection.Nationalities.Single(n => n.Id == Candidate.Nationality);
+        }
+
+        public void Guess(int id)
+        {
+            GuessedNationality = NationalityCollection.Nationalities.SingleOrDefault(x => x.Id == id);
         }
 
         public bool AssessGuess()
